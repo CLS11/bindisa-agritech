cf6ab83d0555dd41b91ec3a9cca6e616512b29bd
crystall.101600 <crystall.101600@gmail.com>
2025-06-16 07:46:49 +0000

    Added multibloc provider for both the main screens.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:agritech/bloc/bug_report_bloc.dart';
 import 'package:agritech/bloc/profile_bloc.dart';
 import 'package:agritech/bloc/profile_event.dart';
+import 'package:agritech/screens/bug_report_main_screen.dart';
 import 'package:agritech/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -19,11 +21,16 @@ class AgriTech extends StatelessWidget {
         primarySwatch: Colors.green,
         fontFamily: 'Inter',
       ),
-      home: BlocProvider(
-        create: (context) => ProfileBloc()..add(
-          LoadProfile(),
-        ),
-        child: const ProfileScreen(),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => ProfileBloc()..add(LoadProfile()),
+          ),
+          BlocProvider(
+            create: (context) => BugReportBloc(), // Provide BugReportBloc
+          ),
+        ], 
+        child: const BugReportMainScreen(),
       ),
     );
   }
