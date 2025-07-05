27cc9a7dbf02e27c241e4b9cb940df611856b4fe
crystall.101600 <crystall.101600@gmail.com>
2025-07-05 14:33:44 +0000

    Sets up the app's theme and provides all BLoC instances globally for various screens.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:agritech/bloc/approval_bloc.dart';
 import 'package:agritech/bloc/bug_report_bloc.dart';
 import 'package:agritech/bloc/chat_message_bloc.dart';
 import 'package:agritech/bloc/notification_bloc.dart';
 import 'package:agritech/bloc/profile_bloc.dart';
 import 'package:agritech/bloc/profile_event.dart';
 import 'package:agritech/bloc/video_bloc.dart';
+import 'package:agritech/screens/approval_flags_screen.dart';
 import 'package:agritech/screens/bug_report_main_screen.dart';
 import 'package:agritech/screens/help_support_screen.dart';
 import 'package:agritech/screens/notifications_screen.dart';
@@ -43,8 +45,11 @@ class AgriTech extends StatelessWidget {
           BlocProvider(
             create: (context) => VideoBloc(), // Provide VideoBloc
           ),
+          BlocProvider(
+            create: (context) => ApprovalBloc(), // Provide ApprovalBloc
+          ),
         ], 
-        child: const HelpSupportScreen(),
+        child: const ApprovalFlagsScreen(),
       ),
     );
   }
